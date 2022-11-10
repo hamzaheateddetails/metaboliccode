24d4814de974098a710492162a4b1a4c54514916
Syed Hamza Hussain <117910559+hamzaheateddetails@users.noreply.github.com>
2022-11-10 18:36:51 +0500

    plugin part 3

---
+++ wp-content/plugins/dynamic-visibility-for-elementor/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'b49a566aad84d2309fc3892aa2ccd0599fddb5ae',
+        'name' => '__root__',
+        'dev' => false,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'b49a566aad84d2309fc3892aa2ccd0599fddb5ae',
+            'dev_requirement' => false,
+        ),
+    ),
+);
